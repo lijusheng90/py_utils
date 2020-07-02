1ab1bf631df73ca22913caeacc33ae489c6c1311
lijusheng <lijusheng@yiducloud.cn>
2020-07-02 14:26:42 +0800

    age

--- util/datetime_tools.py
+++ util/datetime_tools.py
@@ -15,3 +15,18 @@ def check_input_datetime_scope(self, expos_date_scope):
             or not is_valid_date(expos_date_scope[1], "%Y-%m-%d %H:%M:%S"):
         return False
     return True
+
+
+def calculate_age(date_of_birth, date_format="%Y-%m-%d"):
+    today = datetime.today()
+    # 故意不捕获异常，调用方需要处理，防止掩盖错误
+    date_of_birth = datetime.strptime(date_of_birth, date_format)
+    try:
+        birthday = date_of_birth.replace(year=today.year)
+    except ValueError:
+        birthday = date_of_birth.replace(year=today.year, day=date_of_birth.day-1)
+    age = today.year - date_of_birth.year
+    if birthday > today:
+        age -= 1
+    return max(1, age)
+
